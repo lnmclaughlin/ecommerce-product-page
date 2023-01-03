a22ebed7bee922e1c10ac0dc6178f16c1a396b35
Laurie McLaughlin <lnmclaughlin@gmail.com>
2023-01-03 12:40:53 -0500

    updates

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -21,46 +21,49 @@ const Product = ({ specificProduct }: Props) => {
       >
         {specificProduct.image}
       </img>
+      <div className="specific-product-content">
+        <>
+          <h3>SNEAKER COMPANY </h3>
+          <h2> {specificProduct.name} </h2>
+          <p>{specificProduct.description}</p>
+          <div className="price-container">
+            <p className="discounted-price">
+              ${specificProduct.discountPrice!.toFixed(2)}
+            </p>
+            <p className="discount">{specificProduct.discount!}%</p>
+            <p className="original-price">
+              ${specificProduct.originalPrice!.toFixed(2)}
+            </p>
+          </div>
+          <div className="counter-container">
+            <img
+              className="plus"
+              src="/images/icon-plus.svg"
+              alt="add"
+              onClick={() => {}}
+            ></img>
+            <span className="counter">0</span>
+            <img
+              className="minus"
+              src="/images/icon-minus.svg"
+              alt="remove"
+              onClick={() => {}}
+            ></img>
+          </div>
 
-      <h3>SNEAKER COMPANY</h3>
-      <h2>{specificProduct.name}</h2>
-      <p>{specificProduct.description}</p>
-      <div className="price-container">
-        <p className="discounted-price">
-          ${specificProduct.discountPrice!.toFixed(2)}
-        </p>
-        <p className="discount">{specificProduct.discount!}%</p>
-        <p className="original-price">
-          ${specificProduct.originalPrice!.toFixed(2)}
-        </p>
+          <button className="add" onClick={() => addItem(specificProduct)}>
+            Add to Cart
+          </button>
+          {isThisInOrder && (
+            <img
+              src="images/icon-delete.svg"
+              alt="remove"
+              className="remove"
+              onClick={() => removeItem(specificProduct.id)}
+            ></img>
+          )}
+        </>
       </div>
-
-      <div className="counter-container">
-        <img
-          className="plus"
-          src="/images/icon-plus.svg"
-          alt="add"
-          onClick={() => {}}
-        ></img>
-        <span className="counter">0</span>
-        <img
-          className="minus"
-          src="/images/icon-minus.svg"
-          alt="remove"
-          onClick={() => {}}
-        ></img>
-      </div>
-      <button className="add" onClick={() => addItem(specificProduct)}>
-        Add to Cart
-      </button>
-      {isThisInOrder && (
-        <img
-          src="images/icon-delete.svg"
-          alt="remove"
-          className="remove"
-          onClick={() => removeItem(specificProduct.id)}
-        ></img>
-      )}
     </div>
   );
 };
